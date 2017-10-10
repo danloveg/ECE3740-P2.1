47b3116302d01eaae43435d77c913c0c396f1ff4
Daniel Lovegrove <d.lovegrove11@gmail.com>
2017-10-10 09:48:19 -0500

    Create new single threaded client application

---
+++ Client/src/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+/**
+ *
+ * @author loveboat
+ */
+public class Client {
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
